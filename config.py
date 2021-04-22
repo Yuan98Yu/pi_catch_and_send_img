530e737f2ce45a4ccdd10a71e91bcf569bf573bc
YuanYu <951080662@qq.com>
2021-04-22 04:40:26 +0100

    Get it!

---
+++ config.py
@@ -0,0 +1,4 @@
+algorithm_server_ip = '192.168.137.1'
+algorithm_server_port = '8765'
+
+ws_algorithm_server_url = f'ws://{algorithm_server_ip}:{algorithm_server_port}'
---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+import websockets
+import base64
+import time
+
+from camera_pi import Camera
+
+from config import ws_algorithm_server_url
+
+
+def main():
+    camera = Camera()
+    while True:
+        time.sleep(20)
+        asyncio.get_event_loop().run_until_complete(get_and_send_img(camera))
+
+
+async def get_and_send_img(camera: Camera):
+    async with websockets.connect(ws_algorithm_server_url) as websocket:
+        img_bytes = camera.get_frame()
+        img_str = img_bytes
+
+        await websocket.send(img_str)
+
+
+if __name__ == '__main__':
+    main()
